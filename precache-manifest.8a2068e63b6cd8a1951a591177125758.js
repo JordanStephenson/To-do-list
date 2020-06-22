2139a5a6a1b599467081ddbc862a80665d19d146
jordanStephenson <jordan_535@hotmail.com>
2020-06-22 07:38:51 +0100

    Updates

---
+++ precache-manifest.8a2068e63b6cd8a1951a591177125758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9d5f336fdbe832c27bdab044fd0f06",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "b31a4aa30ba4309c9185",
+    "url": "/To-do-list/static/css/main.bd740c9e.chunk.css"
+  },
+  {
+    "revision": "63ffa6d9a08039d104b2",
+    "url": "/To-do-list/static/js/2.1f660ca0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-do-list/static/js/2.1f660ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31a4aa30ba4309c9185",
+    "url": "/To-do-list/static/js/main.fdc42f97.chunk.js"
+  },
+  {
+    "revision": "d9924bae88e1079551da",
+    "url": "/To-do-list/static/js/runtime-main.17841008.js"
+  }
+]);
